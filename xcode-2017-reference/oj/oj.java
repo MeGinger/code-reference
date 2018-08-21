02c3a6ddf36161ffa6a2cf412c7f82f3bfe32c53
shuashuashua123 <john.liu115@gmail.com>
2018-08-21 08:44:34 -0400

    add more

--- xcode-2017-reference/oj/oj.java
+++ xcode-2017-reference/oj/oj.java
@@ -4670,17 +4670,17 @@ public class SentenceSimilarityII {
 	* "(a)())()" -> ["(a)()()", "(a())()"]
 	* ")(" -> [""]
 	*/
-	
+
   /** DFS:
 	* We all know how to check a string of parentheses is valid using a stack. Or even simpler use a counter.
-	* The counter will increase when it is â€˜(â€˜ and decrease when it is â€˜)â€™. Whenever the counter is negative, we have more â€˜)â€™ than â€˜(â€˜ in the prefix.
+	* The counter will increase when it is '(' and decrease when it is ')'. Whenever the counter is negative, we have more ')' than '(' in the prefix.
 	*
-	* To make the prefix valid, we need to remove a â€˜)â€™. The problem is: which one? The answer is any one in the prefix. However, if we remove any one, we will generate duplicate results, for example: s = ()), we can remove s[1] or s[2] but the result is the same (). Thus, we restrict ourself to remove the first ) in a series of concecutive )s.
+	* To make the prefix valid, we need to remove a ')'. The problem is: which one? The answer is any one in the prefix. However, if we remove any one, we will generate duplicate results, for example: s = ()), we can remove s[1] or s[2] but the result is the same (). Thus, we restrict ourself to remove the first ) in a series of concecutive )s.  -- remove any one is the same
 	*
-	* After the removal, the prefix is then valid. We then call the function recursively to solve the rest of the string. However, we need to keep another information: the last removal position. If we do not have this position, we will generate duplicate by removing two â€˜)â€™ in two steps only with a different order.
-	* For this, we keep tracking the last removal position and only remove â€˜)â€™ after that.
+	* After the removal, the prefix is then valid. We then call the function recursively to solve the rest of the string. However, we need to keep another information: the last removal position. If we do not have this position, we will generate duplicate by removing two ')' in two steps only with a different order.
+	* For this, we keep tracking the last removal position and only remove ')' after that.
 	*
-	* Now one may ask. What about â€˜(â€˜? What if s = â€˜(()(()â€™ in which we need remove â€˜(â€˜?
+	* Now one may ask. What about '('? What if s = "(()(()" in which we need remove '('?
 	* The answer is: do the same from right to left.
 	* However a cleverer idea is: reverse the string and reuse the code!
 	* O(n)
